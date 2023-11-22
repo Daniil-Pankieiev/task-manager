57080590ea960a6e2ecf6e7bd8fd72e1fd01d1df
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-11-22 14:34:28 +0200

    Creation app task and creeate models and migrate

---
+++ task/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from task_manager.settings import AUTH_USER_MODEL
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    # def get_absolute_url(self):
+    #     return reverse("taxi:driver-detail", kwargs={"pk": self.pk})
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("Low", "Low"),
+        ("Medium", "Medium"),
+        ("High", "High"),
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default="LOW",
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(AUTH_USER_MODEL, related_name="tasks")
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def __str__(self):
+        return self.name

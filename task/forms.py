bb6220e3893758ee4b204798bb09dcaae5af69c9
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-11-23 20:56:45 +0200

    Add views for models and form for search

---
+++ task/forms.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from task.models import Task, Worker
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+    def clean_deadline(self):
+        if self.cleaned_data["deadline"] < datetime.now().date():
+            raise ValidationError("Deadline can not be assigned earlier than today")
+        return self.cleaned_data["deadline"]
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "position",
+            "first_name",
+            "last_name",
+        )
+
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=150,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by username"}
+        )
+    )
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={"placeholder": "Search by name"}
+        )
+    )
+
+
+class TaskTypeSearchForm(forms.Form):
+
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search"}),
+    )
+
+
+class PositionSearchForm(forms.Form):
+
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search"}),
+    )
